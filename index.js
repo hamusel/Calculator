4fef29f2cc7bb264d778cc549a54751d464a3f5b
Alexandru Iulian Gheorghita <gheorghita@cip.ifi.lmu.de>
2024-09-26 01:07:18 +0200

    :poop: Writing logic for calculator

--- index.js
+++ index.js
@@ -14,15 +14,52 @@ function subtract(a, b) {
     return a - b;
 }
 
-function operate(operator, num1, num2) {
+const display = document.querySelector("#display");
+
+
+function eval(operator, num1, num2) {
     switch (operator) {
-        case "+": return num1 + num2;
-        case "-": return num1 - num2;
-        case "*": return num1 * num2;
-        case "/": return num1 / num2;
+        case "+":
+            display.innerHTML = add(num1, num2);
+            return num1 + num2;
+        case "-":
+            display.innerHTML = subtract(num1, num2);
+            return num1 - num2;
+        case "*":
+            display.innerHTML = multiply(num1, num2);
+            return num1 * num2;
+        case "/":
+            display.innerHTML = divide(num1, num2);
+            return num1 / num2;
     }
 }
 
-let num1 = 0;
+
+let num1 = NaN;
 let num2 = 0;
-let operator = ["+", "-", "*", "/"]
+let operator = "";
+//let operator = ["+", "-", "*", "/"]
+
+
+const numbers = document.querySelectorAll(".number");
+numbers.forEach(button => {
+    button.addEventListener("click", () => {
+        display.textContent = button.innerText;
+        if (isNaN(num1) && operator === "") {
+            num1 = button.innerText;
+        } else if (!isNaN(num2) && operator !== "") {
+            num2 = button.innerText;
+            eval(operator, num1, num2);
+        }
+    })
+})
+
+const operators = document.querySelectorAll(".operator");
+numbers.forEach(button => {
+    button.addEventListener("click", () => {
+        if (!isNaN(num1)) {
+            display.textContent = button.innerText;
+            operator = button.innerText;
+        }
+    })
+})
