24130b028bda48ae37feef53d83fd5641a834eb5
Alexandru Iulian Gheorghita <gheorghita@cip.ifi.lmu.de>
2024-09-27 20:50:06 +0200

    :sparkles: Calculator now works for 2 numbers!

--- index.js
+++ index.js
@@ -20,36 +20,53 @@ const display = document.querySelector("#display");
 function eval(operator, num1, num2) {
     switch (operator) {
         case "+":
-            display.innerHTML = add(num1, num2);
-            return num1 + num2;
+            return add(num1, num2);
         case "-":
-            display.innerHTML = subtract(num1, num2);
-            return num1 - num2;
+            return subtract(num1, num2);
         case "*":
-            display.innerHTML = multiply(num1, num2);
-            return num1 * num2;
+            return multiply(num1, num2);
         case "/":
-            display.innerHTML = divide(num1, num2);
-            return num1 / num2;
+            return divide(num1, num2);
     }
 }
 
-let displayNumber = "";
+let displayNumber1 = "";
+let displayNumber2 = "";
 let num1 = NaN;
-let num2 = 0;
+let num2 = NaN;
 let operator = "";
-//let operator = ["+", "-", "*", "/"]
 
 
 const numbers = document.querySelectorAll(".number");
 numbers.forEach(button => {
     button.addEventListener("click", () => {
-        displayNumber += button.innerText;
-        display.textContent = displayNumber;
 
         if (isNaN(num1)) {
-            num1 = parseFloat(displayNumber);
+            displayNumber1 += button.innerText;
+            display.textContent = displayNumber1;
+            num1 = parseFloat(displayNumber1);
         }
+
+        else {
+            displayNumber2 = displayNumber1;
+            displayNumber2 += button.innerText;
+            display.textContent = displayNumber2;
+            num2 = parseFloat(button.innerText);
+        }
+
+        // displayNumber1 += button.innerText;
+        // display.textContent = displayNumber1;
+        //
+        // if (isNaN(num1)) {
+        //     num1 = parseFloat(displayNumber1);
+        //     alert("num1 is now" + num1);
+        // } else if (!isNaN(num1) && operator !== "") {
+        //     num2 = parseFloat(button.innerText);
+        //     alert("num2 is now" + num2);
+        // }
+
+        // Am un num1? Daca nu, il voi construi pana primesc un operator
+        // Am un num1? Daca da, voi construi un al doilea numar pana la egal/operator
 
     })
 })
@@ -57,10 +74,27 @@ numbers.forEach(button => {
 const operators = document.querySelectorAll(".operator");
 operators.forEach(button => {
     button.addEventListener("click", () => {
-        if (!isNaN(num1)) {
-            operator = button.innerText;
-            displayNumber += operator;
-            display.textContent = displayNumber;
+        operator = button.innerText;
+
+        if (isNaN(displayNumber1[displayNumber1.length - 1])) {
+            displayNumber1 = displayNumber1.slice(0, -1) + operator;
+        } else {
+            displayNumber1 += operator;
         }
-    })
+
+        display.textContent = displayNumber1;
+    });
+});
+
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", () => {
+    console.log(num1)
+    console.log(num2)
+    console.log(operator)
+    if (!isNaN(num1) && !isNaN(num2) && operator !== "") {
+        displayNumber1 = eval(operator, num1, num2);
+        num1 = parseFloat(displayNumber1);
+        display.textContent = displayNumber1;
+    }
+
 })
